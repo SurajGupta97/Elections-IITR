802cdeadbd85597931fac986c966bbfa584f0319
Punit Dhoot <punitdhoot1@gmail.com>
2015-10-04 22:40:52 +0530

    Added submit button to the form and the post method to views

--- voting/views.py
+++ voting/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Votes
+from django.http import HttpResponseRedirect
 
 def index(request):
 	return render(request, 'voting/index.html')
@@ -11,6 +12,16 @@ def details(request , vote_id):
 		position = "General Secretary UG Academics"
 		candidates = ["Punit Dhoot" , "Suraj Gupta" , "Jhatu Shah" , "Personality Wala"]
 		context = {'position' : position , 'candidates': candidates}
+		print Votes.objects.get(user_id =1).vote1
 		return render(request , 'voting/vote.html' , context)
 	elif request.method=="POST":
-		
+		column = 'vote' + `int(vote_id)`
+		print column
+		try:
+			candidate_voted = int(request.POST['vote'])
+			vote = Votes(user_id=candidate_voted)
+			setattr(vote , column, True)
+			vote.save()
+			return HttpResponseRedirect('/vote/2')
+		except:
+			pass
