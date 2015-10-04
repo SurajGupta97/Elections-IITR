d48d75559ab3c427564b08a8af6415030e4f199b
Punit Dhoot <punitdhoot1@gmail.com>
2015-10-04 21:20:40 +0530

    Added certain views to support voting

--- voting/views.py
+++ voting/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Votes
 
-# Create your views here.
+def index(request):
+	return render(request, 'voting/index.html')
+
+def details(request , vote_id):
+	if request.method=="GET":
+		#application logic to get the standing position and the avaiable candidates
+		#default
+		position = "General Secretary UG Academics"
+		candidates = ["Punit Dhoot" , "Suraj Gupta" , "Jhatu Shah" , "Personality Wala"]
+		context = {'position' : position , 'candidates': candidates}
+		return render(request , 'voting/vote.html' , context)
+	elif request.method=="POST":
+		
