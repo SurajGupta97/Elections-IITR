6c259ec54890df1550f4ec865458bf020ed19d13
Suraj <sgshining@gmail.com>
2015-10-04 22:00:19 +0530

    Made some changes to authentication views

--- Elections_IITR/urls.py
+++ Elections_IITR/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^vote/', include('voting.urls')),
+    url(r'^authentication/', include('authentication.urls')),
 ]
