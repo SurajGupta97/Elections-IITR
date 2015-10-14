c780697b74bd18074083899d61d7840af623a833
Punit Dhoot <punitdhoot1@gmail.com>
2015-10-15 01:32:21 +0530

    kuch to kiya

--- voting/views.py
+++ voting/views.py
@@ -11,53 +11,61 @@ except:
 import os
 
 def index(request):
-	config = ConfigParser()
-	config.read(os.path.join(os.path.abspath(os.path.dirname(__file__)) , 'config.cfg'))
-	start = int(config.get("elections" , "start"))
-	if start==0:
-		messages.success(request , "The elections have not yet started! :p")
-		return HttpResponseRedirect('/')
-	return render(request, 'voting/index.html')
+	if request.user.is_authenticated():
+		request.session["username"] = request.user.username
+		config = ConfigParser()
+		config.read(os.path.join(os.path.abspath(os.path.dirname(__file__)) , 'config.cfg'))
+		start = int(config.get("elections" , "start"))
+		if start==0:
+			messages.success(request , "The elections have not yet started! :p")
+			return HttpResponseRedirect('/')
+		return render(request, 'voting/index.html')
+	else:
+		return HttpResponseRedirect('/authentication/signup')
 
 def details(request , vote_id):
-	config = ConfigParser()
-	config.read(os.path.join(os.path.abspath(os.path.dirname(__file__)) , 'config.cfg'))
-	start = int(config.get("elections" , "start"))
-	if start==0:
-		messages.success(request , "The elections have not yet started! :p")
-		return HttpResponseRedirect('/')
+	if request.user.is_authenticated():
+		request.session["username"] = request.user.username
+		config = ConfigParser()
+		config.read(os.path.join(os.path.abspath(os.path.dirname(__file__)) , 'config.cfg'))
+		start = int(config.get("elections" , "start"))
+		if start==0:
+			messages.success(request , "The elections have not yet started! :p")
+			return HttpResponseRedirect('/')
 
-	column = 'vote' + str(vote_id)
-	url = '/vote/' + str(int(vote_id)+1)
-	if request.method=="GET":
-		#application logic to get the standing position and the avaiable candidates
-		#default
-		if Votes.objects.filter(user_id = request.user.id).exists():
-			vote = Votes.objects.get(user_id = request.user.id)
-			val = getattr(vote, column)
-			if val is True:
-				return HttpResponseRedirect(url)
+		column = 'vote' + str(vote_id)
+		url = '/vote/' + str(int(vote_id)+1)
+		if request.method=="GET":
+			#application logic to get the standing position and the avaiable candidates
+			#default
+			if Votes.objects.filter(user_id = request.user.id).exists():
+				vote = Votes.objects.get(user_id = request.user.id)
+				val = getattr(vote, column)
+				if val is True:
+					return HttpResponseRedirect(url)
 
-		position = Positions.objects.get(pk=int(vote_id)).position
-		candidates = Candidate.objects.all().filter(post=int(vote_id))
-		users = []
-		for candidate in candidates:
-			users.append(User.objects.get(id = candidate.user_id))
-		context = {'position' : position , 'candidates': users}
-		return render(request , 'voting/vote.html' , context)
-	elif request.method=="POST":
-		candidate_voted = int(request.POST['vote'])
-		candidate = Candidate.objects.get(user_id = candidate_voted)
-		v = candidate.votes
-		v+=1
-		candidate.votes = v
-		candidate.save()
-		try:
-			vote = Votes.objects.get(user_id = request.user.id)
-			setattr(vote, column , True)
-			vote.save()
-		except:
-			vote = Votes(user_id=request.user.id)
-			setattr(vote , column, True)
-			vote.save()
-		return HttpResponseRedirect(url)
+			position = Positions.objects.get(pk=int(vote_id)).position
+			candidates = Candidate.objects.all().filter(post=int(vote_id))
+			users = []
+			for candidate in candidates:
+				users.append(User.objects.get(id = candidate.user_id))
+			context = {'position' : position , 'candidates': users}
+			return render(request , 'voting/vote.html' , context)
+		elif request.method=="POST":
+			candidate_voted = int(request.POST['vote'])
+			candidate = Candidate.objects.get(user_id = candidate_voted)
+			v = candidate.votes
+			v+=1
+			candidate.votes = v
+			candidate.save()
+			try:
+				vote = Votes.objects.get(user_id = request.user.id)
+				setattr(vote, column , True)
+				vote.save()
+			except:
+				vote = Votes(user_id=request.user.id)
+				setattr(vote , column, True)
+				vote.save()
+			return HttpResponseRedirect(url)
+	else:
+		return HttpResponseRedirect('/authentication/signup')
